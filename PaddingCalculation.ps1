28d9e681fcd850f35971e6c8b6b6469bb1525c7c
Mikołaj Bartnicki <mikolaj@bartnicki.org>
2020-12-13 23:55:18 +0100

    Update script

--- PaddingCalculation.ps1
+++ PaddingCalculation.ps1
@@ -3,8 +3,8 @@
 
 for ($($width = 960; $vpadding = 120; $hpadding = 160); $width -ge 640; $($width -= 10; $vpadding -= 3; $hpadding -= 4)) {
     if ($width -eq 960) {
-        Write-Output "`n`@media only screen and `(max-width: ${width}px`) `{`n`tdiv.content `{`n`t`tmargin: 0 auto`;`n`t`tpadding: ${vpadding}px ${hpadding}px`;`n`t`}`n`}"
+        Write-Output "`r`n`@media only screen and `(max-width: ${width}px`) `{`r`n`tdiv.content `{`r`n`t`tmargin: 0 auto`;`r`n`t`tpadding: ${vpadding}px ${hpadding}px`;`r`n`t`}`r`n`}"
     } else {
-        Write-Output "`n`@media only screen and `(max-width: ${width}px`) `{`n`tdiv.content `{`n`t`tpadding: ${vpadding}px ${hpadding}px`;`n`t`}`n`}"
+        Write-Output "`r`n`@media only screen and `(max-width: ${width}px`) `{`r`n`tdiv.content `{`r`n`t`tpadding: ${vpadding}px ${hpadding}px`;`r`n`t`}`r`n`}"
     }
 }
