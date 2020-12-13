2480153d23ad4d8509a5d29035593433028c5d18
Mikołaj Bartnicki <mikolaj@bartnicki.org>
2020-12-13 23:37:14 +0100

    Update script

--- PaddingCalculation.ps1
+++ PaddingCalculation.ps1
@@ -1,7 +1,7 @@
 ﻿for ($($width = 960; $vpadding = 120; $hpadding = 160); $width -ge 640; $($width -= 10; $vpadding -= 3; $hpadding -= 4)) {
     if ($width -eq 960) {
-        Write-Host `n`@media only screen and `(max-width: ${width}px`) `{`n`tdiv.content `{`n`t`tmargin: 0 auto`;`n`t`tpadding: ${vpadding}px ${hpadding}px`;`n`t`}`n`}
+        Write-Output "`n`@media only screen and `(max-width: ${width}px`) `{`n`tdiv.content `{`n`t`tmargin: 0 auto`;`n`t`tpadding: ${vpadding}px ${hpadding}px`;`n`t`}`n`}"
     } else {
-        Write-Host `n`@media only screen and `(max-width: ${width}px`) `{`n`tdiv.content `{`n`t`tpadding: ${vpadding}px ${hpadding}px`;`n`t`}`n`}
+        Write-Output "`n`@media only screen and `(max-width: ${width}px`) `{`n`tdiv.content `{`n`t`tpadding: ${vpadding}px ${hpadding}px`;`n`t`}`n`}"
     }
 }
